912b53da4012ad183881197e01f8e4a76004a691
Wesley Bernardes <bw3sley@gmail.com>
2023-06-20 21:35:49 -0300

    :sparkles: feat: solved cause and effect challenge

---
+++ 03-cause-n-effect/solved.tsx
@@ -0,0 +1,49 @@
+// Causa vs Efeito
+import { useEffect, useState } from "react"
+
+interface User {
+  name: string;
+  github: string;
+}
+
+function fetchUser() {
+  return {
+    data: {
+      user: {
+        name: 'Joseph Oliveira',
+        github: 'https://github.com/josepholiveira'
+      }
+    }
+  }
+}
+
+export function UserProfile() {
+  const [isFetchingUserProfileInfo, setIsFetchingUserProfileInfo] = useState(false)
+  const [userData, setUserData] = useState<User>()
+
+  useEffect(() => {
+    function loadUser() {
+      setIsFetchingUserProfileInfo(true)
+
+      const fetchUserResponse = fetchUser()
+
+      setUserData(fetchUserResponse.data.user)
+      
+      setIsFetchingUserProfileInfo(false)
+    }
+
+    loadUser()
+  })
+
+  if (isFetchingUserProfileInfo) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      <img src={`${userData?.github}.png`} alt="" />
+      <a href={userData?.github}>{userData?.name}</a>
+    </div>
+  )
+}
+
